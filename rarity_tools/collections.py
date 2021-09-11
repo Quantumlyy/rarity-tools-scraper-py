e2a84d9302ddb4b64ed96cefa934ee6e3369b614
Nejc Drobnič <ethoarx@gmail.com>
2021-09-11 10:51:04 +0200

    refactor: mark most entries as optional and defaulted

--- rarity_tools/collections.py
+++ rarity_tools/collections.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import Optional, Dict
 
 import requests
 from dataclasses_json import dataclass_json
@@ -14,21 +15,41 @@ ua = UserAgent()
 @dataclass
 class ProjectLookup:
 	name: str
-	itemName: str | None
-	added: str | None
-	addedOrder: int | None
-	dynamic: int | None
-	manual: bool | None
-	customHeader: str | None
-	contracts: list[str] | None
-	cv: int | None
+	addedOrder: Optional[int] = None
+	dynamic: Optional[int] = None
+	manual: Optional[bool] = None
+	contracts: Optional[list[str]] = None
+	cv: Optional[int] = None
+	itemName: Optional[str] = None
+	added: Optional[str] = None
+	customHeader: Optional[str] = None
 
 
 @dataclass_json
 @dataclass
 class Projects:
 	list: list[str]
-	lookup: dict  # [str, ProjectLookup]
+	lookup: Dict[str, ProjectLookup]
+
+
+@dataclass_json
+@dataclass
+class CollectionStats:
+	one_day_volume: Optional[float] = None
+	one_day_change: Optional[float] = None
+	one_day_sales: Optional[float] = None
+	one_day_average_price: Optional[float] = None
+	seven_day_volume: Optional[float] = None
+	seven_day_change: Optional[float] = None
+	seven_day_sales: Optional[float] = None
+	seven_day_average_price: Optional[float] = None
+	total_volume: Optional[float] = None
+	total_sales: Optional[float] = None
+	total_supply: Optional[float] = None
+	num_owners: Optional[float] = None
+	average_price: Optional[float] = None
+	market_cap: Optional[float] = None
+	floor_price: Optional[float] = None
 
 
 @dataclass_json
@@ -37,19 +58,20 @@ class Collection:
 	id: str
 	slug: str
 	name: str
-	# TODO: convert to date time
-	created_date: str
 	description: str
-	discord_url: str | None
-	external_url: str | None
-	image_url: str | None
-	large_image_url: str | None
-	banner_image_url: str | None
-	medium_username: str | None
-	twitter_username: str | None
-	instagram_username: str | None
 
-	stats: dict
+	discord_url: Optional[str] = None
+	external_url: Optional[str] = None
+	image_url: Optional[str] = None
+	large_image_url: Optional[str] = None
+	banner_image_url: Optional[str] = None
+	medium_username: Optional[str] = None
+	twitter_username: Optional[str] = None
+	instagram_username: Optional[str] = None
+
+	# TODO: convert to date time
+	created_date: Optional[str] = None
+	stats: Optional[CollectionStats] = None
 
 
 @dataclass_json
@@ -63,5 +85,10 @@ def get_all_collections() -> Collections:
 	res = requests.get(COLLECTIONS_ENDPOINT_URL, headers={
 		"User-Agent": ua.random
 	})
+	json = res.json()
 
-	return Collections.from_dict(res.json())
+	collections = Collections.from_dict(json)
+	for i in range(len(json["collections"])):
+		collections.collections[i] = Collection.from_dict(json["collections"][i])
+
+	return collections
