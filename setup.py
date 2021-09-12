9866f4381cae1c2c9581329042aead1165df02b1
Nejc Drobnič <ethoarx@gmail.com>
2021-09-12 17:30:07 +0200

    feat: add publish files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+from setuptools import setup
+
+setup(
+    name="rarity-tools-scraper",
+    version="0.0.1",
+    url="https://github.com/quantumlyy/rarity-tools-scraper-py",
+    author="Nejc Drobnič",
+    author_email="yo@quantumly.dev",
+    license="BSD-3-Clause",
+    packages=["rarity_tools_scraper"],
+    include_package_data=True,
+    setup_requires="setuptools-pipfile",
+    use_pipfile=True,
+)
