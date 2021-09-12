8c5b227e79ae2d486002a43648fd1765bd0bdf77
Nejc Drobnič <ethoarx@gmail.com>
2021-09-12 18:00:40 +0200

    feat: data endpoint mapping

---
+++ rarity_tools_scraper/data.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from typing import Optional, Dict
+
+import requests
+from dataclasses_json import dataclass_json
+from fake_useragent import UserAgent
+
+BASE_COLLECTION_DATA_ENDPOINT_URL = "https://data.rarity.tools/{endpoint}/{collection}"
+ua = UserAgent()
+
+
+@dataclass_json
+@dataclass
+class Prices:
+	prices: Dict[int, list]
+	v: int
+	startDate: Optional[int] = None
+
+
+def get_collection_prices(collection: str):
+	res = requests.get(BASE_COLLECTION_DATA_ENDPOINT_URL.format(endpoint="prices", collection=collection), headers={
+		"User-Agent": ua.random
+	})
+	json = res.json()
+
+	prices = Prices.from_dict(json)
+
+	return prices
